9647e839c49cc948143946bc56c03acfcda74fe9
Ben Pauza <Benpauza@gmail.com>
2024-05-27 13:48:47 -0500

    Initial Commit

---
+++ Backdraft Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Backdraft Controller
+//
+//  Created by Ben Pauza on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Backdraft ControllerUITests/Backdraft_ControllerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Backdraft_ControllerUITestsLaunchTests.swift
+//  Backdraft ControllerUITests
+//
+//  Created by Ben Pauza on 5/27/24.
+//
+
+import XCTest
+
+final class Backdraft_ControllerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
